7d579f5b55678a7bf93c9498080a220eb3c819c0
irakli302 <iraklidzandzava5@gmail.com>
2026-07-17 18:12:13 +0400

    Added checker for the existence of a file and its parent

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -1,9 +1,11 @@
+import java.io.File;
+
 public class LogAnalyzer {
     public static void main(String[] args){
         String inputPath;
         String outputPath;
 
-        if(args.length > 2){
+        if(args.length >= 2){
             inputPath = args[0];
             outputPath = args[1];
         }
@@ -11,6 +13,20 @@ public class LogAnalyzer {
             inputPath = "logs/server.log";
             outputPath = "reports/summary_report.txt";
             System.out.println("No arguments provided. Using default.");
+        }
+
+        File outFile = new File(outputPath);
+        File outParentFile = outFile.getParentFile();
+        if(outParentFile != null && !outParentFile.exists()){
+            System.out.println("Parent folder doesn`t exists for report file!");
+            System.out.println("Creating missing output directory: " + outParentFile.getPath());
+
+
+            boolean parent = outParentFile.mkdirs();
+            if(!parent) {
+                System.out.println("Error: Could not create directory: " + outParentFile.getPath());
+                System.exit(1);
+            }
         }
     }
 }
