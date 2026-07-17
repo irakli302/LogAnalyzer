dd73fb181c03d843852e72df0916671b07acb573
irakli302 <iraklidzandzava5@gmail.com>
2026-07-17 17:35:02 +0400

    added input/output arguments checker

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -1,5 +1,16 @@
 public class LogAnalyzer {
     public static void main(String[] args){
+        String inputPath;
+        String outputPath;
 
+        if(args.length > 2){
+            inputPath = args[0];
+            outputPath = args[1];
+        }
+        else {
+            inputPath = "logs/server.log";
+            outputPath = "reports/summary_report.txt";
+            System.out.println("No arguments provided. Using default.");
+        }
     }
 }
