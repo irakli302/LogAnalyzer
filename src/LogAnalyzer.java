939ee3a096519ab94599e925ea131c4f53514db9
irakli302 <iraklidzandzava5@gmail.com>
2026-07-18 23:19:03 +0400

    Implemented LogAnalyzer class

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 
 public class LogAnalyzer {
     public static void main(String[] args){
@@ -15,8 +16,11 @@ public class LogAnalyzer {
             System.out.println("No arguments provided. Using default.");
         }
 
+        File inputFile = new File(inputPath);
         File outFile = new File(outputPath);
         File outParentFile = outFile.getParentFile();
+
+
         if(outParentFile != null && !outParentFile.exists()){
             System.out.println("Parent folder doesn`t exists for report file!");
             System.out.println("Creating missing output directory: " + outParentFile.getPath());
@@ -27,6 +31,26 @@ public class LogAnalyzer {
                 System.out.println("Error: Could not create directory: " + outParentFile.getPath());
                 System.exit(1);
             }
+        }
+
+
+        System.out.println("Checking system permissions...");
+        if(!PermissionChecker.checkPermissions(inputFile, outFile))
+            System.exit(1);
+
+
+        System.out.println("✅ Security check passed.");
+        System.out.println("Starting log analysis on: " + inputPath);
+
+
+        try (ReportWriter reportWriter = new ReportWriter(outFile)){
+            LogParser logParser = new LogParser();
+            LogParser.LogAnalysisResult result = logParser.parseLog(inputFile, reportWriter);
+            reportWriter.printSummary(result.totalLines, result.infoCount, result.warningCount, result.errorCount);
+            System.out.println("Safe analysis complete! Hardware sync executed. Report saved to: " + outputPath);
+        }catch (IOException e){
+            System.err.println("Critical error during analysis: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
