9e49613e116ce3defedd42272cd8a9a8b952c46d
irakli302 <iraklidzandzava5@gmail.com>
2026-07-18 22:45:45 +0400

    Implemented ReportWriter class

--- src/ReportWriter.java
+++ src/ReportWriter.java
@@ -2,4 +2,48 @@ import javax.imageio.IIOException;
 import java.io.*;
 
 public class ReportWriter {
+    public FileOutputStream fos;
+    public BufferedWriter writer;
+
+    public ReportWriter(File outputFile) throws FileNotFoundException {
+        this.fos = new FileOutputStream(outputFile);
+        this.writer = new BufferedWriter(new OutputStreamWriter(this.fos));
     }
+
+    public void writeErrorLine(String line) {
+        try {
+            this.writer.write("[CRITICAL ERROR] -> " + line);
+            this.writer.newLine();
+
+            this.writer.flush();
+
+            FileDescriptor fd = fos.getFD();
+            fd.sync();
+        } catch (SyncFailedException e){
+            System.err.println("Hardware disk sync failed: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void printSummary(long totalLines, long infoCount, long warningCount, long errorCount){
+        try {
+            this.writer.write("===============================");
+            this.writer.newLine();
+            this.writer.write("       LOG ANALYSIS SUMMARY");
+            this.writer.write("===============================");
+            this.writer.newLine();
+            this.writer.write("Total Log Lines Processed: " + totalLines);
+            this.writer.newLine();
+            this.writer.write("INFO Messages: " + infoCount);
+            this.writer.newLine();
+            this.writer.write("WARNING Messages: " + warningCount);
+            this.writer.newLine();
+            this.writer.write("ERROR Messages: " + errorCount);
+            this.writer.write("===============================");
+            this.writer.flush();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
